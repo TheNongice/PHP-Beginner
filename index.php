f21432498bc492e4db41a2367e159fcdc6292f73
Wasawat Junnasaksri <ice7533678@gmail.com>
2021-05-10 21:21:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB-Conn Test</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+    body{
+      font-family: 'Ubuntu', sans-serif;
+    }
+    .a{
+      text-align: center;
+    }
+    </style>
+    <!--SweetAlert2-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+</head>
+<body>
+<?php include_once('conn.php'); ?>
+<div class="a">
+<h1>Hello World!</h1>
+<p>This is DB test pages!</p>
+</div>
+</body>
+</html>
