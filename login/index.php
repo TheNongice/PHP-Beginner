3b5e64676e7952c4170a8482f390c986c5c4b8a9
Wasawat Junnasaksri <ice7533678@gmail.com>
2021-05-11 21:18:29 +0700

    Remove Project ID (Fontawesome key leaks)

--- login/index.php
+++ login/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Pages!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/7b30317d32.js" crossorigin="anonymous"></script>
+    <!--<script src="https://kit.fontawesome.com/**YOUR_PROJECT_ID**.js" crossorigin="anonymous"></script> -->
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Mitr&display=swap');
     body{
