b81fdff52fc47dd9fe1b87226b0f7168969589b8
Wasawat Junnasaksri <ice7533678@gmail.com>
2022-03-23 22:54:48 +0700

    Create box_tell.php

---
+++ cookie/msg_name/include/box_tell.php
@@ -0,0 +1,11 @@
+<center>
+    <?php $Text=$_COOKIE['Text-Teller']?>
+    <h1>สวัสดี <?php echo 'คนน่ารักกก >w<';?></h1>
+    <h2>แต่ว่านะ... <?php echo "<h1 style='color: red;'>$Text</h1>"; ?></h2>
+    <h2>น่ารักกว่า</h2>
+    <hr>
+    <form action="/clear.php">
+        <p>ใช้เสร็จอย่าลืมลบเราทิ้งนะ ;-;</p>
+        <button type="submit">ลบประวัติ</button>
+    </form>
+</center>
