9bc778ee5f96ed8a25143a92a1b6f49e29ae96ba
Wasawat Junnasaksri <ice7533678@gmail.com>
2022-03-23 22:54:29 +0700

    Create box_input.php

---
+++ cookie/msg_name/include/box_input.php
@@ -0,0 +1,8 @@
+<center>
+    <h1>สวัสดี _____!</h1>
+    <h4>กรอกชื่อสักคนที่ไม่ใช่ชื่อตัวเองหน่อยสิ ;-;</h4>
+    <form action="/" method="post">
+        <input type="text" name="tell"><br><hr>
+        <button type="submit">กดเลย!</button>
+    </form>
+</center>
